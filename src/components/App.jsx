a9eb3e34fb824983606960fb737fead62c7b5bf6
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-09-03 11:30:10 -0700

    feat: create scaffold for conditional rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,25 +8,38 @@ import { Footer } from './Footer';
 
 export function App() {
 	const [data, setData] = useState([]);
+	const [selectedPiece, setSelectedPiece] = useState(null);
 
 	async function onSearchSubmit(query) {
 		const { data } = await searchArtworks(query);
 		setData(data);
 	}
 
+	function handleArtworkClick(id) {
+		setSelectedPiece(id);
+	}
+
 	return (
 		<div className="App">
 			<h1>TCL Career Lab Art Finder</h1>
-			<SearchForm onSearchSubmit={onSearchSubmit} />
-			<ul>
-				{data.map((artwork) => (
-					<li key={artwork.id}>
-						<a href={`#${artwork.id}`}>
-							{artwork.title} by {artwork.artist_title}
-						</a>
-					</li>
-				))}
-			</ul>
+			{selectedPiece === null && (
+				<>
+					<SearchForm onSearchSubmit={onSearchSubmit} />
+					<ul>
+						{data.map((artwork) => (
+							<li key={artwork.id}>
+								<a
+									href={`#${artwork.id}`}
+									onClick={() => handleArtworkClick(artwork.id)}
+								>
+									{artwork.title} by {artwork.artist_title}
+								</a>
+							</li>
+						))}
+					</ul>
+				</>
+			)}
+			{selectedPiece !== null && <p>Hellaur {selectedPiece}</p>}
 
 			<Footer />
 		</div>
